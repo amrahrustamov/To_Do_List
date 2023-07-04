7d2833adabba2013213aefee743d59004b702156
Asus <Asus@DESKTOP-IJEPJQM>
2023-07-04 16:42:18 +0400

    to do list completed

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const ulList = document.querySelector("ul");
-const liTag = document.querySelector("li");
-const edit = document.querySelector(".edit");
 
-button.addEventListener("click",  function () {
-    if(input.value.trim().length) {
-        ulList.innerHTML += `<li>${input.value} 
+button.addEventListener("click", function () {
+    if (input.value.trim().length) {
+        ulList.innerHTML += `<li><p>${input.value}</p>
+        <input class="newInput" type="text" placeholder=" add new text" />
         <button class="edit">Edit</button>
         <button class="delete">Delete</button></li>`;
         input.value = "";
 
+        const editButtons = document.querySelectorAll(".edit");
+        editButtons.forEach((editButton) => {
+            editButton.addEventListener("click", (event) => {
+                const element = event.target.parentNode.querySelector(".newInput");
+                if (element.style.display === "none") {
+                  element.style.display = "block";
+                } else {
+                  element.style.display = "none";
+                }
+
+                const liElement = event.target.parentNode;
+                const pElement = liElement.querySelector("p");
+                const newText = element.value;
+                if (newText) {
+                    pElement.textContent = newText;
+                }
+                
+            });
+        });
+
+        const deleteButtons = document.querySelectorAll(".delete");
+        deleteButtons.forEach((deleteButton) => {
+            deleteButton.addEventListener("click", (event) => {
+                const liElement = event.target.parentNode;
+                liElement.remove();
+            });
+        });
     }
 });
-liTag.addEventListener("click",  function () {
-   console.log(liTag.value);
-});
-
-
-
